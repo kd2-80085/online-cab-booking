72f23b89119c9dbc68dc7d4b59f469a1843b3a50
kd2-80097 <chetanarote5@gmail.com>
2024-02-08 01:02:02 +0530

    Entities updated with associations and helper methods

--- spring_boot_backend_template/spring_boot_backend_template/src/main/java/com/app/booktaxi/entity/Customer.java
+++ spring_boot_backend_template/spring_boot_backend_template/src/main/java/com/app/booktaxi/entity/Customer.java
@@ -1,18 +1,25 @@
 package com.app.booktaxi.entity;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import java.util.List;
+import java.util.Set;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 /*
  * customer
@@ -25,6 +32,7 @@ id       name     email     password     mob.    booking_id
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = {"bookings","trips"})
 public class Customer extends BaseEntity {
 	
 	
@@ -44,7 +52,12 @@ public class Customer extends BaseEntity {
 	@OneToMany(mappedBy = "customer",cascade= CascadeType.ALL, orphanRemoval = true)
 	private List<Booking> bookings = new ArrayList<>();
 
-	public void addBookings(Booking b)	{
+	@ManyToMany
+    @JoinTable(name = "Employee_Project", joinColumns = @JoinColumn(name="customer_id",nullable = false),inverseJoinColumns = @JoinColumn(name="trip_id",nullable = false))
+    private Set<Trip> trips = new HashSet<>();
+	
+	public void addBookings(Booking b)	
+	{
 		this.bookings.add(b);
 		b.setCustomer(this);
 	}
@@ -60,13 +73,6 @@ public class Customer extends BaseEntity {
 		this.password = password;
 		this.mobile = mobile; 
 	}
-	
-	@Override
-	public String toString() {
-		return "Customer [id=" +getId()+ ", name=" + name + ", email=" + email + ", password=" + password + ", mobile=" + mobile
-				+ ", bookings=" + bookings + "]";
-	}
-	
 	
 	
 }
--- spring_boot_backend_template/spring_boot_backend_template/src/main/java/com/app/booktaxi/entity/Feedback.java
+++ spring_boot_backend_template/spring_boot_backend_template/src/main/java/com/app/booktaxi/entity/Feedback.java
@@ -2,12 +2,16 @@ package com.app.booktaxi.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 /*
  * Feedback
@@ -20,21 +24,25 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = {"booking","driver"})
 public class Feedback extends BaseEntity {
 
 	@Column(name = "customer_id")
 	private int customerId;
+	
+	@ManyToOne
+	@JoinColumn(name = "driver_id")
+	private Driver driver;
+	
+	@OneToOne
+	@JoinColumn(name = "booking_id")
+	private Booking booking;
 	
 	@Column(length = 50)
 	private String feedback;
 	
 	@Column(name = "rating")
 	private int rating;
-
-	@Override
-	public String toString() {
-		return "Feedback [id=" +getId()+ ", customerId=" + customerId + ", feedback=" + feedback + ", rating=" + rating + "]";
-	}
 	
 	
 }
